ca913562de3a2d9e555db9271a1c32ec5b901942
jebi2420 <jebi2420@gmail.com>
2024-09-10 15:48:26 +0900

    [add] add some codes
    finished the "Get Started" tutorial!

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
 function setup() {
-  createCanvas(400, 400);
+  // Creates a canvas 600 pixels wide
+  createCanvas(600, 400);
+  
+  // slow the frame rate
+  frameRate(10);
 }
 
 function draw() {
-  background("pink");
+  // sky blue background
+  background(135, 206, 235);
+
+  // sun
+  fill("yellow");
+  stroke("orange");
+  strokeWeight(20)
+  circle(550, 50, 100);
+
+  // grass on bottom half
+  stroke(0); // black outline
+  strokeWeight(1);
+  fill("green");
+  rect(0,200,600,200)
+
+  // emojis
+  textSize(75);
+  // text("🍎", mouseX, mouseY);
+  text("🍎", 200, 200);
+  text("☁️", 300, 80)
+
+  // line
+  line(pmouseX, pmouseY, mouseX, mouseY);
+
 }
